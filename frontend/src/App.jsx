c318d16a233dc3a12000b7dfe6441d8a46b37a55
ziqin030 <ziqin030@gmail.com>
2025-12-05 01:18:42 +0800

    GPT改的架構

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,28 +6,35 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Global from "./pages/Global";
 import Rag from "./pages/Rag";
-import Login from "./pages/Login";
-import Sign from "./pages/Sign";
+import Login from "./pages/Login"; 
+import Sign from "./pages/Sign"; 
+
 
 function App() {
   return (
-    <>
+    <Router>
+      {/* ⭐ Header 永遠顯示 */}
       <Header />
 
       <main id="main-content">
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/global" element={<Global />} />
-            <Route path="/rag" element={<Rag />} />
-          </Routes>
-        </Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/global" element={<Global />} />
+          <Route path="/rag" element={<Rag />} />
+
+          {/* ⭐ 新增 login 路由 */}
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign" element={<Sign />} />
+
+          {/* 可選：404 */}
+          <Route path="*" element={<div style={{ padding: 40 }}>404 Not Found</div>} />
+        </Routes>
       </main>
 
+      {/* ⭐ Footer 永遠顯示 */}
       <Footer />
-    </>
+    </Router>
   );
 }
 
 export default App;
-
