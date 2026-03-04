4831d3f7e7d7a063df8c1833babffcd1c7028adb
1x919 <ethan930919@gmail.com>
2026-03-05 01:35:36 +0800

    也是大小寫

--- frontend/src/components/Globe1.jsx
+++ frontend/src/components/Globe1.jsx
@@ -3,7 +3,7 @@ import * as THREE from "three";
 import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { OrbitControls, Text } from "@react-three/drei";
 import hierarchy from "../data/hierarchy.json";
-import demandData from "../data/demand_ratio_yearly.json";
+import demandData from "../data/Demand_ratio_yearly.json";
 
 /* ===================== */
 const RADIUS = 2;
