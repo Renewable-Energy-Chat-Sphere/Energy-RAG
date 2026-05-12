cde329d1f506005dfa9c6ae7158d7dbfbc7262ba
Lapis Lazuli <a0912245389@gmail.com>
2026-05-13 03:06:28 +0800

    改 API

--- frontend/src/pages/Rag.jsx
+++ frontend/src/pages/Rag.jsx
@@ -355,8 +355,8 @@ export default function Rag() {
   const [loading, setLoading] = useState(false);
   const [selectedFileName, setSelectedFileName] = useState("");
   
-  // const API = "/api";
-  const API = "http://127.0.0.1:8000";
+  const API = "/api";
+  // const API = "http://127.0.0.1:8000";
   
   async function generateFile(reportData = structuredData) {
     if (!reportData) {
